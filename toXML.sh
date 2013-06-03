9720ea7a9fabc5ee45a28580961b30db316491df
Henry S. Thompson <ht@inf.ed.ac.uk>
2013-06-03 19:28:21 +0100

    work around ED status pblm in XHTML

--- toXML.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-sed '4s/charset="/name="Content-Type" content="charset: /
-     s/<section\([^>]*class="\)/<div\1section /g
-     s/<\(section\|figure\|figcaption\)/<div class="\1"/g
-     s/<\/\(section\|figure\|figcaption\)>/<\/div>/' "$@" | ntidy -utf8 -asxml | \
-sed '1s@.*@<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">@'
-
